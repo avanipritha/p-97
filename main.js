b9f6cb8d97e213df057740f55ca64d9cb049618b
avanipritha <105854907+avanipritha@users.noreply.github.com>
2022-12-19 16:18:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,66 @@
+function start() 
+{
+    window.location = "q.1 screen.html";
+}
+
+function lakshman()
+{
+    window.location = "q.2 screen.html";
+}
+
+function op1945()
+{
+    window.location = "q.3screen.html";
+}
+function babur()
+{
+    window.location = "q.4 screen.html";
+}
+function divine()
+{
+    window.location = "q.5 screen.html";
+}
+function greek()
+{
+    window.location = "q.6 screen.html";
+}
+
+function malyalam()
+{
+    window.location = "q.7 screen.html";
+}
+
+function dvc()
+{
+    window.location = "q.8 screen.html";
+}
+
+function jamesC()
+{
+    window.location = "theend screen.html";
+}
+
+function joe()
+{
+    window.location = "index.html";
+}
+
+function satya()
+{
+    window.location = "index.html";
+}
+
+function taika()
+{
+    window.location = "index.html";
+}
+
+function tryagain()
+{
+    window.location = "index.html"
+}
+
+function restart()
+{
+    window.location = "index.html"
+}
